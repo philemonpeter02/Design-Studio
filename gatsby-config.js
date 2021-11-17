25cc24248dc14fcfb0f074fe786eeb178542b695
Philemon <you@example.com>
2021-11-17 18:56:25 +0500

    update v1.2.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@ module.exports = {
     title: "Sajids Design Studio",
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
@@ -16,18 +15,5 @@ module.exports = {
       },
       __key: "images",
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Sajid's Design Studio | Phil's`,
-        short_name: `Design-Studio`,
-        start_url: `/`,
-        background_color: `#000`,
-        theme_color: `#000`,
-        display: `minimal-ui`,
-        icon: `src/images/logo.png`, 
-      },
-    },
-     `gatsby-plugin-offline`,
   ],
 };
