cd1505542a6a902551d5a67913595fdf6e6cc802
philemonpeter02 <77021404+philemonpeter02@users.noreply.github.com>
2021-11-19 19:52:14 +0500

    Create product.js

---
+++ src/pages/product.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import ProductNav from '../components/ProductNav'
+import ProductDetails from '../components/ProductDetails'
+import ItemCardsSection from '../components/ItemCardsSection'
+import { allData } from '../data/AllData'
+import Footer from "../components/Footer"
+
+const Product = ({location}) => {
+    return (
+        <Layout>
+          <ProductNav title={location.state ? location.state.title : null} downloads={location.state ? location.state.downloads : null} views={location.state ? location.state.views : null} />
+          <ProductDetails img={location.state ? location.state.img : null}  />
+          <ItemCardsSection itemData={allData}/>
+          <Footer />
+        </Layout>
+    )
+}
+
+export default Product
